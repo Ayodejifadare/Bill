78bb641d9683a54ba4a26ab82a74b89757f5af98
Ayodejifadare <ayordejidaniel@gmail.com>
2025-08-22 17:36:33 +0100

    feat: add recurring bill split persistence

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ import receiptRoutes from './routes/receipts.js'
 import spendingInsightsRoutes from './routes/spendingInsights.js'
 import recurringPaymentRoutes from './routes/recurringPayments.js'
 import { cleanupExpiredCodes } from './utils/verificationCodes.js'
+import { scheduleRecurringBillSplits } from './utils/recurringBillSplitScheduler.js'
 
 // Load environment variables
 dotenv.config()
@@ -36,6 +37,9 @@ const UPLOAD_DIR = process.env.AVATAR_UPLOAD_DIR || 'uploads'
 
 // Initialize Prisma Client
 const prisma = new PrismaClient()
+
+// Setup recurring bill split scheduler
+scheduleRecurringBillSplits(prisma)
 
 // Periodically remove expired verification codes
 setInterval(() => cleanupExpiredCodes(prisma), 60 * 60 * 1000)
